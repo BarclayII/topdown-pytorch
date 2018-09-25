f9adf2ab147ad780da8591df0d9bbf8b3a206be5
Gan Quan <coin2028@hotmail.com>
2018-09-24 22:27:13 -0400

    oops

--- modules.py
+++ modules.py
@@ -127,7 +127,7 @@ class CommNet(nn.Module):
 
     def forward(self, fm):
         shape = fm.shape
-        hs = fm.view(shape[0], 1, -1).repeat(1, self.n_branches, 1).unbind(1)
+        hs = fm.view(shape[0], 1, -1).repeat(1, self.n_branches, 1)
         for i in range(self.n_steps):
             h_sum = hs.sum(dim=-2, keepdim=True)
             cs = (h_sum - hs) / (1e-8 + self.n_branches - 1)
